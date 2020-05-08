415bc470a566c115e99c9d803ebb973ec22d91ed
ranim-ha <ranimhankal97m@gmail.com>
2020-05-08 16:17:38 +0300

    0

---
+++ HOTEL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HOTEL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
